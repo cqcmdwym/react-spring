96e9f0d808b640c667e30318701e359e9d8eb427
Cookie Chen <cookie.chen@autodesk.com>
2019-12-30 11:14:43 +0800

    first commit

---
+++ src/main/java/com/greglturnquist/reactSpring/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.greglturnquist.reactSpring;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+}
